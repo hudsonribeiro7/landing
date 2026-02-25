d6f3a3fcb5cb46b326257ddb15c0ce93ee4414a5
Hudson Ribeiro <hudsonriberio7@gmail.com>
2026-02-25 18:25:02 -0300

    novo stilo
    ainda falta muito para ficar profissional

--- script.js
+++ script.js
@@ -3,3 +3,49 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Obrigado pelo contato, Hudson vai responder em breve!");
 });
+
+
+const carousel = document.querySelector('.carousel');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+carousel.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 2; // velocidade do arrasto
+  carousel.scrollLeft = scrollLeft - walk;
+});
+
+// Suporte para touch (mobile)
+carousel.addEventListener('touchstart', (e) => {
+  isDown = true;
+  startX = e.touches[0].pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('touchend', () => {
+  isDown = false;
+});
+
+carousel.addEventListener('touchmove', (e) => {
+  if (!isDown) return;
+  const x = e.touches[0].pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 2;
+  carousel.scrollLeft = scrollLeft - walk;
+});
